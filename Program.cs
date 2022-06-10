4c60525e1ce699691cbd0f7ddc75c812d099f482
unknown <nester669@gmail.com>
2022-06-10 20:30:42 +0300

    Add program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void
+Print(string[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.Write("Введите количество элементов массива: ");
+
+int num = int.Parse(Console.ReadLine());
+
+string[] array = new string[num];
+
+Console.WriteLine("Введите элементы массива через Enter: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Console.ReadLine();
+}
+
+Console.WriteLine();
+
+
+int index = 0;
+
+string[] array2 = new string[num];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        array2[index] = array[i];
+        index++;
+    }
+}
+
+Print(array2);
